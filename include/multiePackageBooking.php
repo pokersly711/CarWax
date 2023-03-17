ba6bc7b1df2f89ffd8457fba91eeef1a3dc1283f
james Russell <jrussell83@my.devry.edu>
2023-03-16 20:14:47 -0500

    file change
    Changed file names and content. (1) betSpecial- added content and changed name for file navigation,.

---
+++ include/multiePackageBooking.php
@@ -0,0 +1,261 @@
+		
+
+		<li>
+			
+			<!-- Content -->
+			<div class="template-component-booking-item-content">
+				
+				<!-- Package list -->
+				<ul class="template-component-booking-package-list">
+					
+					<!-- Package -->
+					<li data-id="basic-hand-wash-1" data-id-vehicle-rel="regular-size-car,compact-suv,minivan,pickup-truck,cargo-truck">
+						
+						<!-- Header -->
+						<h4 class="template-component-booking-package-name">Invest in your car and future. We offer 10, 30, 50 exterior shines for a discounted price below our regular price.</h4>
+						
+						<!-- Price -->
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">10</span>
+							<span class="template-component-booking-package-price-total">exterior</span>
+							<span class="template-component-booking-package-price-decimal">Shines</span>
+						</div>
+						
+						<!-- Duration -->
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration">$</span>
+							<span class="template-component-booking-package-duration-value">400</span>
+							<span class="template-component-booking-package-duration-unit">00</span>
+						</div>
+						
+						<!-- Services -->
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior and Trim Wash</li>
+							<li data-id="towel-hand-dry">Hand Polish</li>
+							<li data-id="wheel-shine">and Wax.</li>
+						</ul>
+						
+						<!-- Button -->
+						<div class="template-component-button-box">
+							<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now</a>
+						</div>
+						
+					</li>
+					
+					<!-- Package -->
+					<li data-id="basic-hand-wash-2" data-id-vehicle-rel="medium-size-car">
+						
+						<!-- Header -->
+						<h4 class="template-component-booking-package-name">Our 30 shines services</h4>
+						
+						<!-- Price -->
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">$</span>
+							<span class="template-component-booking-package-price-total"></span>
+							<span class="template-component-booking-package-price-decimal">00</span>
+						</div>
+						
+						<!-- Duration -->
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration"></span>
+							<span class="template-component-booking-package-duration-value">60-90</span>
+							<span class="template-component-booking-package-duration-unit">min</span>
+						</div>
+						
+						<!-- Services -->
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior Hand Wash and Wax</li>
+							<li data-id="towel-hand-dry">Towel Hand Dry</li>
+							<li data-id="wheel-shine">Wheel Shine</li>
+						</ul>
+						
+						<!-- Button -->
+						<div class="template-component-button-box">
+							<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now</a>
+						</div>
+						
+					</li>
+					
+					<!-- Package -->
+					<li data-id="deluxe-wash-1" data-id-vehicle-rel="regular-size-car,compact-suv,minivan,pickup-truck,cargo-truck">
+						<h4 class="template-component-booking-package-name">Deluxe Wash</h4>
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">$</span>
+							<span class="template-component-booking-package-price-total">100</span>
+							<span class="template-component-booking-package-price-decimal">00</span>
+						</div>
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration"></span>
+							<span class="template-component-booking-package-duration-value">75</span>
+							<span class="template-component-booking-package-duration-unit">min</span>
+						</div>
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior Hand Wash</li>
+							<li data-id="towel-hand-dry">Towel Hand Dry</li>
+							<li data-id="wheel-shine">Wheel Shine</li>
+							<li data-id="tire-dressing">Tire Dressing</li>
+							<li data-id="windows-in-and-out">Windows In &amp; Out</li>
+							<li data-id="sealer-hand-wax">Sealer Hand Wax</li>
+						</ul>
+						<div class="template-component-button-box">
+							<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now</a>
+						</div>
+					</li>
+					
+					<!-- Package -->
+					<li data-id="deluxe-wash-2" data-id-vehicle-rel="medium-size-car">
+						<h4 class="template-component-booking-package-name">Deluxe Wash</h4>
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">$</span>
+							<span class="template-component-booking-package-price-total">30</span>
+							<span class="template-component-booking-package-price-decimal">95</span>
+						</div>
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration"></span>
+							<span class="template-component-booking-package-duration-value">55</span>
+							<span class="template-component-booking-package-duration-unit">min</span>
+						</div>
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior Hand Wash</li>
+							<li data-id="towel-hand-dry">Towel Hand Dry</li>
+							<li data-id="wheel-shine">Wheel Shine</li>
+							<li data-id="tire-dressing">Tire Dressing</li>
+							<li data-id="windows-in-and-out">Windows In &amp; Out</li>
+							<li data-id="sealer-hand-wax">Sealer Hand Wax</li>
+						</ul>
+						<div class="template-component-button-box">
+							<a href="#" class="template-component-button">Book Now</a>
+						</div>
+					</li>
+					
+					<!-- Package -->
+					<li data-id="ultimate-shine-1" data-id-vehicle-rel="regular-size-car,compact-suv,minivan">
+						<h4 class="template-component-booking-package-name">Ultimate Shine</h4>
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">$</span>
+							<span class="template-component-booking-package-price-total">75</span>
+							<span class="template-component-booking-package-price-decimal">00</span>
+						</div>
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration"></span>
+							<span class="template-component-booking-package-duration-value">60</span>
+							<span class="template-component-booking-package-duration-unit">min</span>
+						</div>
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior Hand Wash</li>
+							<li data-id="towel-hand-dry">Towel Hand Dry</li>
+							<li data-id="wheel-shine">Wheel Shine</li>
+							<li data-id="tire-dressing">Tire Dressing</li>
+							<li data-id="windows-in-and-out">Windows In &amp; Out</li>
+							<li data-id="sealer-hand-wax">Sealer Hand Wax</li>
+							<li data-id="interior-vacuum">Interior Vacuum</li>
+							<li data-id="trunk-vacuum">Trunk Vacuum</li>
+							<li data-id="door-shuts-and-plastics">Door Shuts &amp; Plastics</li>
+							<li data-id="dashboard-clean">Dashboard Clean</li>
+						</ul>
+						<div class="template-component-button-box">
+							<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now</a>
+						</div>
+					</li>
+					
+					<!-- Package -->
+					<li data-id="ultimate-shine-2" data-id-vehicle-rel="medium-size-car">
+						<h4 class="template-component-booking-package-name">Ultimate Shine</h4>
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">$</span>
+							<span class="template-component-booking-package-price-total">52</span>
+							<span class="template-component-booking-package-price-decimal">95</span>
+						</div>
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration"></span>
+							<span class="template-component-booking-package-duration-value">80</span>
+							<span class="template-component-booking-package-duration-unit">min</span>
+						</div>
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior Hand Wash</li>
+							<li data-id="towel-hand-dry">Towel Hand Dry</li>
+							<li data-id="wheel-shine">Wheel Shine</li>
+							<li data-id="tire-dressing">Tire Dressing</li>
+							<li data-id="windows-in-and-out">Windows In &amp; Out</li>
+							<li data-id="sealer-hand-wax">Sealer Hand Wax</li>
+							<li data-id="interior-vacuum">Interior Vacuum</li>
+							<li data-id="trunk-vacuum">Trunk Vacuum</li>
+							<li data-id="door-shuts-and-plastics">Door Shuts &amp; Plastics</li>
+							<li data-id="dashboard-clean">Dashboard Clean</li>
+						</ul>
+						<div class="template-component-button-box">
+							<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now</a>
+						</div>
+					</li>
+					
+					<!-- Package -->
+					<li data-id="platinium-works-1" data-id-vehicle-rel="regular-size-car">
+						<h4 class="template-component-booking-package-name">Platinum Works</h4>
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">$</span>
+							<span class="template-component-booking-package-price-total">65</span>
+							<span class="template-component-booking-package-price-decimal">95</span>
+						</div>
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration"></span>
+							<span class="template-component-booking-package-duration-value">100</span>
+							<span class="template-component-booking-package-duration-unit">min</span>
+						</div>
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior Hand Wash</li>
+							<li data-id="towel-hand-dry">Towel Hand Dry</li>
+							<li data-id="wheel-shine">Wheel Shine</li>
+							<li data-id="tire-dressing">Tire Dressing</li>
+							<li data-id="windows-in-and-out">Windows In &amp; Out</li>
+							<li data-id="sealer-hand-wax">Sealer Hand Wax</li>
+							<li data-id="interior-vacuum">Interior Vacuum</li>
+							<li data-id="trunk-vacuum">Trunk Vacuum</li>
+							<li data-id="door-shuts-and-plastics">Door Shuts &amp; Plastics</li>
+							<li data-id="dashboard-clean">Dashboard Clean</li>
+							<li data-id="air-freshener">Air Freshener</li>
+							<li data-id="triple-coat-hand-wax">Triple Coat Hand Wax</li>
+							<li data-id="exterior-vinyl-protectant">Exterior Vinyl Protectant</li>
+						</ul>
+						<div class="template-component-button-box">
+							<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now</a>
+						</div>
+					</li>
+					
+					<!-- Package -->
+					<li data-id="platinium-works-2" data-id-vehicle-rel="medium-size-car">
+						<h4 class="template-component-booking-package-name">Platinum Works</h4>
+						<div class="template-component-booking-package-price">
+							<span class="template-component-booking-package-price-currency">$</span>
+							<span class="template-component-booking-package-price-total">82</span>
+							<span class="template-component-booking-package-price-decimal">95</span>
+						</div>
+						<div class="template-component-booking-package-duration">
+							<span class="template-icon-booking-meta-duration"></span>
+							<span class="template-component-booking-package-duration-value">125</span>
+							<span class="template-component-booking-package-duration-unit">min</span>
+						</div>
+						<ul class="template-component-booking-package-service-list">
+							<li data-id="exterior-hand-wash">Exterior Hand Wash</li>
+							<li data-id="towel-hand-dry">Towel Hand Dry</li>
+							<li data-id="wheel-shine">Wheel Shine</li>
+							<li data-id="tire-dressing">Tire Dressing</li>
+							<li data-id="windows-in-and-out">Windows In &amp; Out</li>
+							<li data-id="sealer-hand-wax">Sealer Hand Wax</li>
+							<li data-id="interior-vacuum">Interior Vacuum</li>
+							<li data-id="trunk-vacuum">Trunk Vacuum</li>
+							<li data-id="door-shuts-and-plastics">Door Shuts &amp; Plastics</li>
+							<li data-id="dashboard-clean">Dashboard Clean</li>
+							<li data-id="air-freshener">Air Freshener</li>
+							<li data-id="triple-coat-hand-wax">Triple Coat Hand Wax</li>
+							<li data-id="exterior-vinyl-protectant">Exterior Vinyl Protectant</li>
+						</ul>
+						<div class="template-component-button-box">
+							<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now</a>
+						</div>
+					</li>
+					
+				</ul>
+				
+			</div>	
+			
+		</li>
---
+++ page/betaSpecial.php
@@ -0,0 +1,300 @@
+		
+			
+				<!-- Header -->
+				<div class="template-header template-header-background template-header-background-3">
+<?php
+		Template::includeTemplateHeaderTop();
+		Template::includeTemplateHeaderBottom('Our Services',array(array(Template::getPageURL('home',false),'Home'),array('#','Our Services')));
+?>
+				
+				</div>
+
+				
+				<!-- Content -->
+				<div class="template-content">
+					
+					<!-- Section -->
+					<div class="template-section template-section-padding-1 template-clear-fix template-main">
+						
+						<!-- Header + subheader -->
+						<div class="template-component-header-subheader">
+							<h2>Help us work out the kinks. Your convenience is our main focus.</h2>
+							<div></div>
+							<span>Your feedback is needed, and for your time we are gonna offer a special price.</span>
+						</div>		
+						
+						<!-- Text -->
+						<div class="template-align-center"> 
+							<p>
+								Any size personal vehicle for $40.00. All we ask is you give us some feedback on your experience with us. This price will change when we are done with testing every aspect of the service from User Experience on our app to The service we provide.
+							</p>
+						</div>
+						
+						<!-- Space -->
+						<div class="template-component-space template-component-space-2"></div>
+						
+						<!--- Layout 33x33x33% -->
+						<ul class="template-layout-33x33x33 template-clear-fix">
+							
+							<!-- Left column -->
+							<li class="template-layout-column-left">
+								<div class="template-component-image template-component-image-preloader">
+									<a href="<?php Template::getPageURL('single-service-right-sidebar'); ?>">
+										<img src="media/image/sample/460x306/image_05.jpg" alt=""/>
+										<span class="template-component-image-hover"></span>
+									</a>
+								</div>
+								<h5 class="template-margin-top-2">
+									<a href="<?php Template::getPageURL('single-service-right-sidebar'); ?>">
+										Exterior Hand Wash
+									</a>
+								</h5>
+								<p class="template-padding-reset">This package includes hand wash, hand wax, trim protectant, and wheels and tires.</p>
+							</li>
+							
+							<!-- Center column -->
+							<li class="template-layout-column-center">
+								<div class="template-component-image template-component-image-preloader">
+									<a href="<?php Template::getPageURL('single-service-right-sidebar'); ?>">
+										<img src="media/image/sample/460x306/image_06.jpg" alt=""/>
+										<span class="template-component-image-hover"></span>
+									</a>
+								</div>
+								<h5 class="template-margin-top-2">
+									<a href="<?php Template::getPageURL('single-service-right-sidebar'); ?>">
+										We use the best products on the market.
+									</a>
+								</h5>
+								<p class="template-padding-reset">Chemical Guys automotive products is the only product we use.</p>
+							</li>
+							
+							<!-- Right column -->
+							<li class="template-layout-column-right">
+								<div class="template-component-image template-component-image-preloader">
+									<a href="<?php Template::getPageURL('single-service-right-sidebar'); ?>">
+										<img src="media/image/sample/460x306/image_07.jpg" alt=""/>
+										<span class="template-component-image-hover"></span>
+									</a>
+								</div>
+								<h5 class="template-margin-top-2">
+									<a href="<?php Template::getPageURL('single-service-right-sidebar'); ?>">
+										Foam Wash
+									</a>
+								</h5>
+								<p class="template-padding-reset">Phasellus sit amet egestas ex praesent sollicitudinal mattis lacus non facilisis nunc. Quisque consectetura odio ut sagittis consequat nisl.</p>
+							</li>
+							
+						</ul>
+						
+					</div>
+					
+					<!-- Section -->
+					<div class="template-section template-section-padding-reset template-clear-fix">
+					
+						<!-- Flex layout 50x50% -->
+						<div class="template-layout-flex template-background-color-1 template-clear-fix">
+
+							<!-- Left column -->
+							<div class="template-align-center">
+
+								<!--- Header + subheader -->
+								<div class="template-component-header-subheader">
+									<h2>Beta Special</h2>
+									<div></div>
+									<span>Book your Shine Now!</span>
+								</div>
+								
+								<!-- Text -->
+								<p class="template-padding-reset">
+									Make sure to use the button below and book your shine to get this special price. 
+									<br/><br/>
+									Let us know every step of the way how your experience has been, and what we could do to make it a perfect interaction. 
+								</p>
+								
+								<!-- Space -->
+								<div class="template-component-space template-component-space-2"></div>
+								
+								<!-- Button -->
+								<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Book Now!</a>
+								
+							</div>
+
+							<!-- Right column -->
+							<div class="template-background-image template-background-image-3"></div>
+
+						</div>
+						
+						<!-- Flex layout 50x50% -->
+						<div class="template-layout-flex template-background-color-1 template-clear-fix">
+
+							<!-- Left column -->
+							<div class="template-background-image template-background-image-4"></div>
+
+							<!-- Right column -->
+							<div class="template-align-center">
+								
+								<!-- Header + subheader -->
+								<div class="template-component-header-subheader">
+									<h2>Our Team</h2>
+									<div></div>
+									<span>Our appreciation to the ones who have worked so hard to get this company up and running. Who has been dedicated to designing and developing this app and the constant testing to get eveything perfect for release.</span>
+								</div>
+								
+								<!-- Text -->
+								<p class="template-padding-reset">
+									Hyperactive Digital Studios, the lead design team. They worked long hours from branding to final testing. 
+									<br/><br/>
+									Quatilics Inc, who put the time in to help code a majority of the site.
+								</p>
+								
+								<!-- Space -->
+								<div class="template-component-space template-component-space-2"></div>
+								
+								<!-- Button -->
+								<a href="<?php Template::getPageURL('book-your-wash'); ?>" class="template-component-button">Hyperactive Studios</a>
+								
+							</div>
+
+						</div>
+						
+					</div>
+					
+					<!-- Section -->
+					<div class="template-section template-section-padding-1 template-clear-fix template-main">
+						
+						<!-- Header + subheader -->
+						<div class="template-component-header-subheader">
+							<h2>Vehicle Type Packages</h2>
+							<div></div>
+							<span>We offer multiple services</span>
+						</div>
+						
+						<div class="template-align-center">
+						
+							<!-- Tabs -->
+							<div class="template-component-tab">
+								
+								<!-- Navigation -->
+								<ul>
+									<li><a href="#tab-1-small-car">Small Car</a></li>
+									<li><a href="#tab-1-medium-car">Medium Car</a></li>
+									<li><a href="#tab-1-small-suv">Small Suv</a></li>
+								</ul>
+								
+								<!-- Panel 1 -->
+								<div id="tab-1-small-car">
+									<p class="template-padding-reset">
+										Etiam ullamcorper est terminal metro. Suspendisse a novum etos pellentesque a non felis maecenas module vimeo at malesuada. 
+										Primus elit lectus at felis, malesuada ultricies obec curabitur et ligula sande porta node vestibulum une commodo a convallis laoreet enim.
+										Morbi at sinum interdum etos fermentum. Nulla elite terminal integer. Novum etos module un metro laoreet enim, morbi at sinum interdum etos 
+										fermentum in dolor supremum.
+									</p>
+								</div>
+								
+								<!-- Panel 2 -->
+								<div id="tab-1-medium-car">
+									<p class="template-padding-reset">
+										Quisque purus magna commodo a facilisis pulvinar fermentum vel lacus. Phasellus id cursus tortor. Nulla sodales felis in purus convallis 
+										vitae convallis tellus ornare. Sed auctor elit et hendrerit vestibulum nibh ex consectetur purus a aliquet lorem ex nec risus. 
+										In in cursus neque. Cras varius enim eget leo bibendum et pharetra augue rhoncus. Nam blandit elit sit amet tristique imperdiet 
+										urna augue ultrices nisi eu accumsan ex tortor eu libero. Maecenas condimentum neque et neque euismod et ullamcorper est congue. 
+									</p>
+								</div>
+								
+								<!-- Panel 3 -->
+								<div id="tab-1-small-suv">
+									<p class="template-padding-reset">
+										Vivamus nec faucibus mauris. Proin vestibulum dui eu pretium tincidunt ante leo mattis nibh sit amet ultrices ex neque sit amet turpis. 
+										Sed aliquet urna elit et maximus sapien pretium dictum. Integer sit amet sagittis mauris vel placerat quam. Proin finibus orci a lectus 
+										viverra egestas. Donec elementum hendrerit placerat. Donec venenatis suscipit elit facilisis rutrum. Maecenas cursus mauris id commodo 
+										iaculis. Vivamus a est ex.
+									</p>
+								</div>
+								
+							</div>	
+							
+						</div>
+						
+						<!-- Space -->
+						<div class="template-component-space template-component-space-2"></div>
+						
+						<!-- Divider -->
+						<div class="template-component-divider"></div>
+						
+						<!-- Space -->
+						<div class="template-component-space template-component-space-2"></div>
+						
+						<!-- Features list -->
+						<div class="template-component-feature-list template-component-feature-list-position-top">
+							
+							<!-- Layout 25x25x25x25% -->
+							<ul class="template-layout-25x25x25x25 template-clear-fix">
+								
+								<!-- Left column -->
+								<li class="template-layout-column-left">
+									<span class="template-icon-feature-user-chat"></span>
+									<h5>Easy To Reach</h5>
+									<p>Primus combine lectus at node terminal forks craftmanship at gravida enum por etos ultricies curabitur et ligula sanden morbi terminal gravida.</p>
+								</li>
+								
+								<!-- Center left column -->
+								<li class="template-layout-column-center-left">
+									<span class="template-icon-feature-check"></span>
+									<h5>Best Results</h5>
+									<p>Etiam sodales interdum justo id convallis quam sollicitudin pharetra nam gravidam vulputate fringilla fusce tellus erat laorel et enim non posuere iaculis risus.</p>
+								</li>				
+								
+								<!-- Center right column -->
+								<li class="template-layout-column-center-right">
+									<span class="template-icon-feature-eco-car"></span>
+									<h5>Eco Friendly</h5>
+									<p>Nunc hendrerit ipsum nec enim pellenten tempus proin consectetur sapien non leo luctus sed tempus ipsum condimentumm curabitur vel dui molestie.</p>
+								</li>
+								
+								<!-- Right column -->	
+								<li class="template-layout-column-right">
+									<span class="template-icon-feature-oil-gauge"></span>
+									<h5>Oil Change</h5>
+									<p>Ut non iaculis nisi nec gravida diam fusce tincidunt sem id velit placerat semper on risus fringilla. Nam quis neque orci proina tempus semper malesuada.</p>
+								</li>	
+								
+								<!-- Left column -->	
+								<li class="template-layout-column-left">
+									<span class="template-icon-feature-water-drop"></span>
+									<h5>Hand Wash</h5>
+									<p>Curabitur quis lectus vitae nibh pulvinar porttitor in eget neque donec pellentesa ut justo non pulvinar nulla consectetural tincidunt nisi.</p>
+								</li>
+																
+								<!-- Center left column -->
+								<li class="template-layout-column-center-left">
+									<span class="template-icon-feature-vacuum-cleaner"></span>
+									<h5>Interior Clean</h5>
+									<p>Mauris maximus massa id urna feugiami lobortis nunc pulvinar velit ut felis facilis quis euismod sem placerat nulla vel sem vitae eros convallis cursus.</p>
+								</li>
+								
+								<!-- Center right column -->
+								<li class="template-layout-column-center-right">
+									<span class="template-icon-feature-payment"></span>
+									<h5>Online Payments</h5>
+									<p>Etiam gravida leo vitae aliquam vehicula sem ipsum accumsan justo quis volutpat erat lacus a purus sed eu molestie anten ut rhoncus metus at varius tincidunt.</p>
+								</li>	
+								
+								<!-- Right column -->
+								<li class="template-layout-column-right">
+									<span class="template-icon-feature-engine-belt"></span>
+									<h5>Engine Clean</h5>
+									<p>Donec eu tellus vitae velit tinciduntam facilisis aenean nisi nunc condimentum malesuada erat vel mattis auctor diam detiam ut velit vitae justo rhoncus.</p>
+								</li>	
+								
+							</ul>
+							
+						</div>
+						
+					</div>
+					
+					<!-- Google Maps -->
+					<div class="template-section template-section-padding-reset template-clear-fix">
+						<?php Template::includeFile('google-map-1'); ?>
+					</div>
+					
+				</div>
